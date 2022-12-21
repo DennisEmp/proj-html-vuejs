b551ce61c74b6be367c163ebba8c9b464c468b56
Dennis Empirio <dennis.empirio@gmail.com>
2022-12-21 12:06:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+// implement
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+},
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+</style>
